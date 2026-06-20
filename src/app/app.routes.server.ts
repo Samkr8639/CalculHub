843e4f85a89ad17de497c31d4d434203904f863c
Samkr8639 <sameervirak@gmail.com>
2026-06-20 17:08:18 +0530

    update sitemap timestamps

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,14 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { BLOG_POSTS } from './blog/blog-posts.data';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'blog/:slug',
     renderMode: RenderMode.Prerender,
     getPrerenderParams: async () => {
-      return [
-        { slug: 'calculate-bike-loan-emi-guide' },
-        { slug: 'sip-vs-lumpsum-mutual-funds' }
-      ];
+      return BLOG_POSTS.map(post => ({ slug: post.slug }));
     }
   },
   {
