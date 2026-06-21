c3f19d084856e5399865a10130e3fe70adce6bff
Samkr8639 <sameervirak@gmail.com>
2026-06-21 18:29:57 +0530

    feat: install and integrate @vercel/analytics tracking package

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers, provideClientHydration(withEventReplay())]})
   .catch((err) => console.error(err));
